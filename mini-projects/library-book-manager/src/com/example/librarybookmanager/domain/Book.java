ad63588eb7d1747347b48953e9a8d658538c26d5
eziquexx <waftyann@gmail.com>
2025-07-02 21:58:43 +0900

    [librarybookmanager] 25.07.02 : Book 도메인 생성 (제목, 저자, 출판연도) + 생성자 사용하여 도서 등록 + getter로 도서 검색, 저자 검색, 연도 검색 가능

---
+++ mini-projects/library-book-manager/src/com/example/librarybookmanager/domain/Book.java
@@ -0,0 +1,30 @@
+package com.example.librarybookmanager.domain;
+
+public class Book {
+  private String title;
+  private String author;
+  private int year;
+
+  // 도서 등록 객체
+  public Book(String title, String author, int year) {
+    this.title = title;
+    this.author= author;
+    this.year = year;
+  }
+
+  // 책 제목으로 도서 검색
+  public String getTitle() {
+    return title;
+  }
+
+  // 책 저자로 도서 검색
+  public String getAuthor() {
+    return author;
+  }
+
+  // 연도별로 도서 검색
+  public int getYear() {
+    return year;
+  }
+  
+}

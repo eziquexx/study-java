dd2948af9e3af2f98688aca3143b30eade607b4c
eziquexx <waftyann@gmail.com>
2025-07-03 00:39:29 +0900

    [librarybookmanager] 25.07.03 : 애플리케이션 실행 작업

---
+++ mini-projects/library-book-manager/src/com/example/librarybookmanager/LibraryBookManagerApplication.java
@@ -0,0 +1,48 @@
+package com.example.librarybookmanager;
+
+import java.util.Scanner;
+
+import com.example.librarybookmanager.constants.Message;
+import com.example.librarybookmanager.service.LibraryServiceImpl;
+
+public class LibraryBookManagerApplication {
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      LibraryServiceImpl library = new LibraryServiceImpl();
+  
+      System.out.println(Message.TITLE);
+      
+      while (true) {
+        System.out.println(Message.MENU);
+        System.out.print("메뉴를 선택하세요: ");
+        try {
+          int choice = Integer.parseInt(sc.nextLine());
+          
+          switch (choice) {
+            case 1 -> {
+              library.registerBook();
+            }
+            case 2 -> {
+              library.printBook();
+            }
+            case 3 -> {
+              library.searchBook();
+            }
+            case 4 -> {
+              library.deleteBook();
+            }
+            case 5 -> {
+              System.out.println("프로그램을 종료합니다.");
+              return;
+            }
+            default -> System.out.println("올바른 번호를 입력하세요.\n");
+          } 
+  
+        } catch (Exception e) {
+          System.out.println("숫자만 입력해주세요.\n");
+        } 
+      }
+
+    }
+  }
+}

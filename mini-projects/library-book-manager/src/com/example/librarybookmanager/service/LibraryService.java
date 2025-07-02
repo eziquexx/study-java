302abc07c4d412caefcb33e813081a4288b549bb
eziquexx <waftyann@gmail.com>
2025-07-03 01:12:08 +0900

    [librarybookmanager] 25.07.03 : LibraryService 인터페이스 메서드 수정

--- mini-projects/library-book-manager/src/com/example/librarybookmanager/service/LibraryService.java
+++ mini-projects/library-book-manager/src/com/example/librarybookmanager/service/LibraryService.java
@@ -1,9 +1,7 @@
 package com.example.librarybookmanager.service;
 
-import com.example.librarybookmanager.exception.InvalidYearException;
-
 public interface LibraryService {
-  void registerBook() throws InvalidYearException;  // 책 등록
+  void registerBook();  // 책 등록
   void printBook();     // 책 목록
   void searchBook();    // 책 검색
   void deleteBook();    // 책 삭제
